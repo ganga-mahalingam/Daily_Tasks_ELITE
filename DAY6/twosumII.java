9834f5f7a6103cfe15105977f7d301ff865a4960
Ganga Mahalingam <gangamahalingam19@gmail.com>
2026-03-23 14:50:42 +0000

    Create twosumII.java

---
+++ DAY6/twosumII.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i = 0;i < arr.length;i++) {
+            arr[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        int left=0;
+        int right=n-1;
+        while(left<=right){
+            int sum=arr[left]+arr[right];
+            if(sum==target){
+                System.out.println(left+" "+right);
+                break;
+            }
+            else if(sum > target){
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+    }
+}
